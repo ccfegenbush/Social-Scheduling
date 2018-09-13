f8dd1ad442163a27febddf69a68235d060330178
DClarkyD <djclark1111@gmail.com>
2018-09-12 21:34:04 -0400

    added register button

--- frontend-react-redux/src/components/nav/nav.component.tsx
+++ frontend-react-redux/src/components/nav/nav.component.tsx
@@ -24,7 +24,9 @@ const AppNav: React.StatelessComponent<any> = (props) => {
             <li className="nav-item active">
               <Link to="/sign-in" className="unset-anchor nav-link">Sign In</Link>
             </li>
-
+            <li className="nav-item active">
+              <Link to="/register" className="unset-anchor nav-link">Register</Link>
+            </li>
             <li className="nav-item active dropdown">
               <a className="nav-link dropdown-toggle pointer" id="examples-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Examples</a>
               <div className="dropdown-menu" aria-labelledby="examples-dropdown">
