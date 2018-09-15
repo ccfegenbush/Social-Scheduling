0f05bbd86e4357ea14a229390ddfec91b62163b2
Joshua Seitz <seitzjoshua7@gmail.com>
2018-09-15 10:48:09 -0400

    Created masthead, and update style for the home page

---
+++ frontend-react-redux/src/components/masthead/masthead.component.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+export const Masthead = (): any => {
+    return (
+        <header className="masthead">
+            <div className="container">
+                <div className="intro-text">
+                    <div className="intro-lead-in">Welcome To Our Studio!</div>
+                    <div className="intro-heading text-uppercase">It's Nice To Meet You</div>
+                    <a className="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Tell Me More</a>
+                </div>
+            </div>
+        </header>
+    );
+}
