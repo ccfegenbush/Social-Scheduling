524341c89c967017c0b2ef831d2bfd286bc597f9
Joshua Seitz <seitzjoshua7@gmail.com>
2018-09-15 09:56:21 -0400

    import some function from my last project, and change nav logo name

--- frontend-react-redux/src/components/nav/nav.component.tsx
+++ frontend-react-redux/src/components/nav/nav.component.tsx
@@ -7,7 +7,7 @@ const AppNav: React.StatelessComponent<any> = (props) => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
       <div className="container">
-        <a className="navbar-brand js-scroll-trigger">Social</a>
+        <a className="navbar-brand js-scroll-trigger">Social Planit</a>
         <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           Menu
         <i className="fas fa-bars"></i>
