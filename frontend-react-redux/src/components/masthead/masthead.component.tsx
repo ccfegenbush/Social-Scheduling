c70f89551ef3ab754266e0a33771c56c922096df
Joshua Seitz <seitzjoshua7@gmail.com>
2018-09-15 10:50:52 -0400

    Changed the home page masthead text

--- frontend-react-redux/src/components/masthead/masthead.component.tsx
+++ frontend-react-redux/src/components/masthead/masthead.component.tsx
@@ -5,8 +5,8 @@ export const Masthead = (): any => {
         <header className="masthead">
             <div className="container">
                 <div className="intro-text">
-                    <div className="intro-lead-in">Welcome To Our Studio!</div>
-                    <div className="intro-heading text-uppercase">It's Nice To Meet You</div>
+                    <div className="intro-lead-in">Welcome To Social Planit!</div>
+                    <div className="intro-heading text-uppercase">Welcome!</div>
                     <a className="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Tell Me More</a>
                 </div>
             </div>
