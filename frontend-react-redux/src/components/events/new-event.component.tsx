042c07f49dabd735431cba3f16aa693b22c7e947
DClarkyD <djclark1111@gmail.com>
2018-09-15 11:43:38 -0400

    event component with redux mostly working

--- frontend-react-redux/src/components/events/new-event.component.tsx
+++ frontend-react-redux/src/components/events/new-event.component.tsx
@@ -1,17 +1,20 @@
 import * as React from 'react';
-import { environment } from '../../environment';
-import { RouteComponentProps } from 'react-router';
-import * as newUserActions from '../../actions/new-user/new-user.actions';
 import { connect } from 'react-redux';
-import { INewUserState, IState } from '../../reducers';
+import { RouteComponentProps } from 'react-router';
+import * as newEventActions from '../../actions/event/events.actions';
+import { environment } from '../../environment';
+import { INewEventState, IState } from '../../reducers';
 
-interface IProps extends RouteComponentProps<{}>, INewUserState {
-    updateAge: (age: string) => any,
-    updateEmail: (email: string) => any,
-    updateFirstName: (firstName: string) => any,
-    updateLastName: (lastName: string) => any,
-    updatePassword: (password: string) => any,
-    updateUsername: (username: string) => any,
+interface IProps extends RouteComponentProps<{}>, INewEventState {
+    updateEventName: (eventName: string) => any,
+    updateEventType: (eventType: string) => any,
+    updateEventDescription: (eventDescription: string) => any,
+    updateEventStartDate: (eventStartDate: string) => any,
+    updateEventEndDate: (eventEndDate: string) => any,
+    updateEventStartTime: (updateEventStartTime: string) => any,
+    updateEventEndTime: (eventEventEndTime: string) => any,
+    updateEventLocation: (location: string) => any,
+    updateAuthorId: (authorId: number) => any,
     onSubmit: (user: any) => any
 }
 
@@ -20,56 +23,73 @@ class NewEventComponent extends React.Component<IProps, {}> {
         super(props);
     }
 
-    public usernameChange = (e: any) => {
-        this.props.updateUsername(e.target.value);
+    public eventNameChange = (e: any) => {
+        this.props.updateEventName(e.target.value);
     }
 
-    public passwordChange = (e: any) => {
-        this.props.updatePassword(e.target.value);
+    public eventTypeChange = (e: any) => {
+        this.props.updateEventType(e.target.value);
     }
 
-    public firstNameChange = (e: any) => {
-        this.props.updateFirstName(e.target.value);
-    }
-    
-    public lastNameChange = (e: any) => {
-        this.props.updateLastName(e.target.value);
+    public eventDescriptionChange = (e: any) => {
+        this.props.updateEventDescription(e.target.value);
     }
 
-    public ageChange = (e: any) => {
-        this.props.updateAge(e.target.value);
+    public eventStartDateChange = (e: any) => {
+        this.props.updateEventStartDate(e.target.value);
     }
 
-    public emailChange = (e: any) => {
-        this.props.updateEmail(e.target.value);
+    public eventEndDateChange = (e: any) => {
+        this.props.updateEventEndDate(e.target.value);
     }
-    
+
+    public eventStartTimeChange = (e: any) => {
+        this.props.updateEventStartTime(e.target.value);
+    }
+
+    public eventEndTimeChange = (e: any) => {
+        this.props.updateEventEndTime(e.target.value);
+    }
+
+    public eventLocationChange = (e: any) => {
+        this.props.updateEventLocation(e.target.value);
+    }
+
+    public authorIdChange = (e: any) => {
+        this.props.updateAuthorId(e.target.value);
+    }
+
     public onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        const u = this.props;
+        const ev = this.props;
         e.preventDefault();
-        const user = {
-            "age": Number(u.age),
-            "email": u.email,
-            "firstName": u.firstName,
-            "lastName": u.lastName,
-            "password": u.password,
-            "username": u.username
+        const event = {
+            "authorId": JSON.parse(localStorage.getItem('userId') || '{}'),
+            "description": ev.description,
+            "endDate": ev.endDate,
+            "endTime": ev.endTime,
+            "location": ev.eventLocation,
+            "name": ev.name,
+            "startDate": ev.startDate,
+            "startTime": ev.startTime,
+            "type": ev.type,
         }
-        fetch(environment.context + "users/register", {
-            body: JSON.stringify(user),
+
+        console.log(event)
+        fetch(environment.context + "events/create", {
+            body: JSON.stringify(event),
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json'
             },
             method: 'POST'
         })
-        .then(resp => resp.json())
-        .then(userData => {
-            this.props.history.push('/home');
-        })
-        .catch(err => {
-            console.log(err);
-        })
+            .then(resp => resp.json())
+            .then(userData => {
+                this.props.history.push('/home');
+            })
+            .catch(err => {
+                console.log(err);
+            })
     }
 
     public render() {
@@ -77,83 +97,107 @@ class NewEventComponent extends React.Component<IProps, {}> {
         return (
             <div>
                 <form className="form-signin" onSubmit={this.onSubmit}>
-                    <h1 className="h3 mb-3 font-weight-normal">Create a New User</h1>
+                    <h1 className="h3 mb-3 font-weight-normal">Create a New Event</h1>
 
-                    <label htmlFor="inputUsername" className="sr-only">Username</label>
+                    <label htmlFor="inputName" className="sr-only">Name</label>
                     <input
-                    onChange={this.usernameChange}
-                    value={u.username}
-                    type="text"
-                    name="username"
-                    className="form-control"
-                    placeholder="Username"
-                    required />
+                        onChange={this.eventNameChange}
+                        value={u.name}
+                        type="text"
+                        name="event name"
+                        className="form-control"
+                        placeholder="Event Name"
+                        required />
 
-                    <label htmlFor="inputPassword" className="sr-only">Password</label>
+                    <label htmlFor="ev" className="sr-only">Event type</label>
                     <input
-                    onChange={this.passwordChange}
-                    value={u.password}
-                    type="password"
-                    name="password"
-                    className="form-control"
-                    placeholder="Password"
-                    required />
+                        onChange={this.eventTypeChange}
+                        value={u.type}
+                        type="text"
+                        name="eventType"
+                        className="form-control"
+                        placeholder="Event Type"
+                        required />
 
-                    <label htmlFor="inputFirstName" className="sr-only">First Name</label>
+                    <label htmlFor="inputDescription" className="sr-only">Description</label>
                     <input
-                    onChange={this.firstNameChange}
-                    value={u.firstName}
-                    type="text"                    
-                    name="firstName"
-                    className="form-control"
-                    placeholder="First Name"
-                    required />
+                        onChange={this.eventDescriptionChange}
+                        value={u.description}
+                        type="text"
+                        name="description"
+                        className="form-control"
+                        placeholder="Description"
+                        required />
 
-                    <label htmlFor="inputLastName" className="sr-only">Last Name</label>
+                    <label htmlFor="inputStartDate" className="sr-only">Start Date</label>
                     <input
-                    onChange={this.lastNameChange}
-                    value={u.lastName}
-                    type="text"                    
-                    name="lastName"
-                    className="form-control"
-                    placeholder="Last Name"
-                    required />
+                        onChange={this.eventStartDateChange}
+                        value={u.startDate}
+                        type="text"
+                        name="startDate"
+                        className="form-control"
+                        placeholder="Start Date"
+                        required />
 
-                    <label htmlFor="inputAge" className="sr-only">Age</label>
+                    <label htmlFor="inputEndDate" className="sr-only">End Date</label>
                     <input
-                    onChange={this.ageChange}
-                    value={u.age}
-                    type="text"                    
-                    name="age"
-                    className="form-control"
-                    placeholder="Age"
-                    required />
+                        onChange={this.eventEndDateChange}
+                        value={u.endDate}
+                        type="text"
+                        name="endDate"
+                        className="form-control"
+                        placeholder="End Date"
+                        required />
 
-                    <label htmlFor="inputEmail" className="sr-only">Email</label>
+                    <label htmlFor="inputStartTime" className="sr-only">Start time</label>
                     <input
-                    onChange={this.emailChange}
-                    value={u.email}
-                    type="text"                    
-                    name="email"
-                    className="form-control"
-                    placeholder="Email"
-                    required />
+                        onChange={this.eventStartTimeChange}
+                        value={u.startTime}
+                        type="text"
+                        name="start time"
+                        className="form-control"
+                        placeholder="Start Time"
+                        required />
+
+                    <label htmlFor="inputEndTime" className="sr-only">End time</label>
+                    <input
+                        onChange={this.eventEndTimeChange}
+                        value={u.endTime}
+                        type="text"
+                        name="end time"
+                        className="form-control"
+                        placeholder="End Time"
+                        required />
+
+                    <label htmlFor="inputLocation" className="sr-only">Location</label>
+                    <input
+                        onChange={this.eventLocationChange}
+                        value={u.eventLocation}
+                        type="text"
+                        name="location"
+                        className="form-control"
+                        placeholder="Location"
+                        required />
 
                     <button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
                 </form>
-                
+
             </div>
         )
     }
 }
 
-    const mapStateToProps = (state: IState) => (state.newUser);
-    const mapDispatchToProps = {
-        updateAge: newUserActions.updateAge,
-        updateEmail: newUserActions.updateEmail,
-        updateFirstName: newUserActions.updateFirstName,
-        updateLastName: newUserActions.updateLastName,
-        updatePassword: newUserActions.updatePassword,
-        updateUsername: newUserActions.updateUsername
-    }
+const mapStateToProps = (state: IState) => (state.newEvent);
+const mapDispatchToProps = {
+    updateAuthorId: newEventActions.updateAuthorId,
+    updateEventDescription: newEventActions.updateEventDescription,
+    updateEventEndDate: newEventActions.updateEventEndDate,
+    updateEventEndTime: newEventActions.updateEventEndTime,
+    updateEventLocation: newEventActions.updateEventLocation,
+    updateEventName: newEventActions.updateEventName,
+    updateEventStartDate: newEventActions.updateEventStartDate,
+    updateEventStartTime: newEventActions.updateEventStartTime,
+    updateEventType: newEventActions.updateEventType,
+   
+}
 export default connect(mapStateToProps, mapDispatchToProps)(NewEventComponent);
