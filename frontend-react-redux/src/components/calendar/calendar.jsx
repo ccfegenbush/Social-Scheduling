f5a58c9d7f3f7c8ee1e6853b71b42afa1024dd69
Joshua Seitz <seitzjoshua7@gmail.com>
2018-09-17 18:21:42 -0400

    Changed Calendar from function component to class component

--- frontend-react-redux/src/components/calendar/calendar.jsx
+++ frontend-react-redux/src/components/calendar/calendar.jsx
@@ -1,28 +1,41 @@
-import React, { Component } from 'react';
+import * as React from 'react';
 import BigCalendar from 'react-big-calendar';
-import { connect } from 'react-redux';
+// import { connect } from 'react-redux';
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 BigCalendar.setLocalizer(BigCalendar.momentLocalizer(moment));
 
-let events = [
-    {
-        end: new Date(moment().add(1, "days")),
-        start: new Date(),
-        title: "Some title"
-    }
-]
+export default class MyCalendar extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            events: [
+                {
+                    end: new Date(moment().add(1, "days")),
+                    start: new Date(),
+                    title: "Some title"
+                }
+            ]
+        }
 
-const MyCalendar = (props) => {
-    return <div className="mt-5 pt-5 container">
-        <BigCalendar events={events}
-            defaultDate={new Date()}
-            defaultView="month"
-            style={{ height: "100vh" }}
-        />
-    </div>
+    }
+
+    // componentDidMount() {
+        
+    // }
+
+    render() {
+        return <div className="mt-5 pt-5 container">
+            <BigCalendar events={this.state.events}
+                defaultDate={new Date()}
+                defaultView="month"
+                style={{ height: "100vh" }}
+            />
+        </div>
+    }
 
 }
 
-export default MyCalendar;
+// const mapStateToProps = (state) => (state)
+// export default connect(mapStateToProps)(MyCalendar);
