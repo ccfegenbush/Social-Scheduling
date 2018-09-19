a3da4bac0570807ad5f8268c341c98abca1c713a
Chase Fegenbush <chase.fegenbush22@gmail.com>
2018-09-19 16:47:23 -0400

    edit interests fully functional. profile complete

--- frontend-react-redux/src/components/profile/edit-interests/edit-interests.component.tsx
+++ frontend-react-redux/src/components/profile/edit-interests/edit-interests.component.tsx
@@ -27,7 +27,7 @@ export class EditInterestsComponent extends React.Component<any, any> {
     public addInterest = (e:any) => {
         e.preventDefault();
         this.setState((prevState: any) => ({
-            interests: [...prevState.interests, ''],
+            interests: [...prevState.interests, {}],
         }));
     }
 
@@ -41,7 +41,7 @@ export class EditInterestsComponent extends React.Component<any, any> {
     public handleChange = (e:any) => {
         e.preventDefault();
         const interests = [...this.state.interests]   
-        interests[e.target.id] = e.target.value
+        interests[e.target.id] = {id:Number(e.target.value), interest: e.target[Number(e.target.value)].text}
         this.setState({ interests }, () => console.log(this.state.interests))
       }
 
@@ -49,24 +49,21 @@ export class EditInterestsComponent extends React.Component<any, any> {
         e.preventDefault();
         const i = this.state;
         console.log(i.interests);
-        for (const x of i.interests) {
-            console.log(x);
-            fetch(environment.context + `users/${JSON.parse(localStorage.getItem('userId') || '{}')}/addInterest`, {
-                body: JSON.stringify({id: x}),
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                },
-                method: 'POST'
+        fetch(environment.context + `users/${JSON.parse(localStorage.getItem('userId') || '{}')}/updateInterests`, {
+            body: JSON.stringify(i.interests),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            method: 'POST'
+        })
+            .then(resp => resp.json())
+            .then(interestData => {
+                this.props.history.push('/profile');
             })
-                .then(resp => resp.json())
-                .then(interestData => {
-                    this.props.history.push('/home');
-                })
-                .catch(err => {
-                    console.log(err);
-                })
-        }
+            .catch(err => {
+                console.log(err);
+            })
     }
 
     public render() {
@@ -91,9 +88,10 @@ export class EditInterestsComponent extends React.Component<any, any> {
                                         <option value="1">Sports</option>
                                         <option value="2">Movies</option>
                                         <option value="3">Shopping</option>
-                                        <option value="5">Beach</option>
-                                        <option value="6">Traveling</option>
-                                        <option value="7">Gaming</option>
+                                        <option value="4">Beach</option>
+                                        <option value="5">Traveling</option>
+                                        <option value="6">Gaming</option>
+                                        <option value="7">Food</option>
                                     </select>
                                     <button onClick={(e) => this.removeInterest(e, index)}>Remove</button>
                                 </div>
@@ -101,7 +99,7 @@ export class EditInterestsComponent extends React.Component<any, any> {
                         })
                     }
 
-                    <button className="btn btn-lg btn-primary btn-block" type="submit" value="Add Node server">Sign in</button>
+                    <button className="btn btn-lg btn-primary btn-block" type="submit" value="Add Node server">Submit</button>
                     {/* {errorMessage && <p id="error-message">{errorMessage}</p>} */}
                 </form>
             </div>
