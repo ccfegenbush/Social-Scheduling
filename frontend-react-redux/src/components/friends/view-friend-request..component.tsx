82d380a9265fa530bfebb56587efa5bae9df7de3
DClarkyD <djclark1111@gmail.com>
2018-09-20 16:55:25 -0400

    added buttons to approve/deny a friend request

--- frontend-react-redux/src/components/friends/view-friend-request..component.tsx
+++ frontend-react-redux/src/components/friends/view-friend-request..component.tsx
@@ -25,7 +25,7 @@ export class FriendRequestComponent extends React.Component<any, any>  {
                 console.log(this.state.requests)
                 for (const i of this.state.requests) {
                     console.log(i);
-                    const friendId = i.friendId;
+                    const friendId = i.userId;
 
                     fetch(environment.context + `users/${friendId}`, {})
                         .then(resp => resp.json())
@@ -34,7 +34,6 @@ export class FriendRequestComponent extends React.Component<any, any>  {
                                 ...this.state,
                                 friends: [...this.state.friends, friend]
                             })
-
                             console.log(this.state)
                         })
                         .catch(err => {
@@ -45,7 +44,54 @@ export class FriendRequestComponent extends React.Component<any, any>  {
             .catch(err => {
                 console.log(err)
             })
+    }
 
+    public onApprove = (friendId: any, e: any) => {
+        console.log(`approving with id: ${friendId}`)
+        const userId = JSON.parse(localStorage.getItem('userId') || '{}')
+        console.log(`userId: ${userId}`)
+        e.preventDefault();
+        
+        const id = friendId
+        fetch(environment.context + `users/${userId}/addFriend`, {
+            body: JSON.stringify(id),
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+            method: 'Post'
+        })
+            .then(resp => {
+            console.log(resp.status)
+            if (resp.status === 401) {
+            console.log('Invalid request');
+            } else if (resp.status === 200) {
+            return resp.json();
+            } else {
+            console.log('Failed to approve user at this time');
+            }
+            throw new Error('Failed to make friend');
+      })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+
+    public onDeny = (friendId: any, e: any) => {
+        console.log(`denying with id: ${friendId}` )
+        e.preventDefault();
+        fetch(environment.context + `requests/editStatus/${JSON.parse(localStorage.getItem('userId') || '{}')}`, {
+            body: JSON.stringify(friendId),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            method: 'Post'
+        })
+            .then(resp => resp.json())
+            .catch(err => {
+                console.log(err);
+            })
     }
 
     public render() {
@@ -57,6 +103,7 @@ export class FriendRequestComponent extends React.Component<any, any>  {
                         <tr>
                             <th scope="col">Username</th>
                             <th scope="col">Requestor's name</th>
+                            <th scope="col">Accept or Deny</th>
                         </tr>
                     </thead>
                     <tbody id="profile-table-body">
@@ -65,6 +112,14 @@ export class FriendRequestComponent extends React.Component<any, any>  {
                                 <tr key={friend.id} >
                                     <td>{friend.username}</td>
                                     <td>{friend.firstName}</td>
+                                    <td>
+                                        <button onClick={(e) => this.onApprove(friend.id, e)}>
+                                            Accept
+                                        </button>
+                                        <button onClick={(e) => this.onDeny(friend.id, e)}>
+                                            Deny
+                                        </button>
+                                    </td>
                                 </tr>
                             ))
                         }
