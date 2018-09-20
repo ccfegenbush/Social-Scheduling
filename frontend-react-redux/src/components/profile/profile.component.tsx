24d2d2c4acbb047224bb5a090b93a3efac87249f
Joshua Seitz <seitzjoshua7@gmail.com>
2018-09-20 15:11:31 -0400

    Update profile page layout and style

--- frontend-react-redux/src/components/profile/profile.component.tsx
+++ frontend-react-redux/src/components/profile/profile.component.tsx
@@ -5,7 +5,7 @@ import { FaTwitter, FaFacebookF, FaLinkedinIn } from 'react-icons/fa';
 
 
 export class ProfileComponent extends React.Component<any, any>  {
-    
+
     public constructor(props: any) {
         super(props);
         const userJSON = localStorage.getItem("user")
@@ -47,49 +47,51 @@ export class ProfileComponent extends React.Component<any, any>  {
     }
 
     public render() {
-        const img = require('../../assets/profile.png');
+        const img = require('../../assets/profile.jpg');
         const userInfo = this.state.username;
         const listInterests = this.state.interests.map(
-            (p: any) => <li key=
-                {p.interest}>{p.interest}</li>)
+            (p: any) => <li className="list-group-item" key={p.interest}>{p.interest}</li>)
         return (
             <section className="bg-light pt-5" id="team">
                 <div className="container">
                     <div className="row">
                         <div className="col-lg-12 text-center">
                             <h2 className="section-heading text-uppercase">My Profile</h2>
-                            <h3 className="section-subheading text-muted mb-0">{userInfo.username}</h3>
+                            <h3 className="section-subheading text-muted mb-3">{userInfo.username}</h3>
                         </div>
                     </div>
+                    <hr/>
                     <div className="row">
-                        <div className="col-sm-4 offset-md-4">
+                        <div className="col-sm-6 float-right">
                             <div className="team-member">
-                                <img className="mx-auto rounded-circle" src={img} alt="" />
+                                <img className="mx-auto rounded-circle" src={img} />
                                 <h4>{`${userInfo.firstName} ${userInfo.lastName}`}</h4>
                                 <p className="text-muted">{userInfo.email}</p>
                                 <ul className="list-inline social-buttons">
                                     <li className="list-inline-item">
                                         <a href="#">
-                                            <FaTwitter/>
+                                            <FaTwitter />
                                         </a>
                                     </li>
                                     <li className="list-inline-item">
                                         <a href="#">
-                                            <FaFacebookF/>
+                                            <FaFacebookF />
                                         </a>
                                     </li>
                                     <li className="list-inline-item">
                                         <a href="#">
-                                            <FaLinkedinIn/>
+                                            <FaLinkedinIn />
                                         </a>
                                     </li>
                                 </ul>
                             </div>
                         </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-lg-8 mx-auto text-center">
-                            <p className="large text-muted">Interests: {listInterests}</p>
+                        <div className="col-sm-6 mx-auto">
+                            <p className="large text-muted"><strong>My Interests: </strong>                                
+                                <ul className="list-group">
+                                    {listInterests}
+                                </ul>
+                            </p>
                         </div>
                     </div>
                 </div>
