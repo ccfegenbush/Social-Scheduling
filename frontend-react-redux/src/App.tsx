ba5306e23870f5e4ac48a268708c4e475302a098
Chase Fegenbush <chase.fegenbush22@gmail.com>
2018-09-19 22:01:21 -0400

    ...

--- frontend-react-redux/src/App.tsx
+++ frontend-react-redux/src/App.tsx
@@ -14,13 +14,8 @@ import { store } from './Store';
 import { LandingPage } from './views/landingPage';
 import newEventComponent from './components/events/new-event.component';
 import { InterestsFormComponent } from './components/interests/interests-form.component';
-<<<<<<< HEAD
-import editProfileComponent from './components/profile/edit-profile/edit-profile.component';
-import { FriendComponent } from './components/friends/view-friends';
-=======
 import EditProfileComponent from './components/profile/edit-profile/edit-profile.component';
 import { EditInterestsComponent } from './components/profile/edit-interests/edit-interests.component';
->>>>>>> a3da4bac0570807ad5f8268c341c98abca1c713a
 
 class App extends React.Component {
   public state = { value: '' };
@@ -41,13 +36,8 @@ class App extends React.Component {
                 <Route path="/make-event" component={newEventComponent} />
                 <Route path="/events/invitations" component={InvitationComponent} />
                 <Route path="/profile" component={ProfileComponent} />
-<<<<<<< HEAD
-                <Route path="/edit-profile" component={editProfileComponent} />
-                <Route path="/view-friends" component={FriendComponent} />
-=======
                 <Route path="/edit-profile" component={EditProfileComponent} />
                 <Route path="/edit-interests" component={EditInterestsComponent} />
->>>>>>> a3da4bac0570807ad5f8268c341c98abca1c713a
                 <Route path="/" component={LandingPage} />
                 <Route component={LandingPage} />
               </Switch>
