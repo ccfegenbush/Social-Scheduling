e0ce7ab8a6b3ea9d83713f8c64f3e8a2b63c1c39
Josue Rodriguez <jrod_1729@hotmail.com>
2018-09-23 13:41:15 -0400

    some tests

--- frontend-react-redux/src/App.test.tsx
+++ frontend-react-redux/src/App.test.tsx
@@ -1,10 +1,26 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
+import { FriendRequestComponent } from './components/friends/view-friend-request.component';
+import { InterestsFormComponent } from './components/interests/interests-form.component';
+import { AddFriendComponent } from './components/friends/add-friends.component';
+import { InvitationComponent } from './components/events/view-event-invitations.component';
+import {shallow} from 'enzyme';
+import { ViewFriendProfile } from './components/friends/view-friends-profile.component';
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<FriendRequestComponent />, div);
+  ReactDOM.render(<InterestsFormComponent />, div);
+  ReactDOM.render(<AddFriendComponent />, div);
+  ReactDOM.render(<InvitationComponent />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('renders without crashing', () => {
+  shallow(<ViewFriendProfile />);
+});
+
+
+
 
